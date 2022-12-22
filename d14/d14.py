7afcf30651de5d3b07652bc03c5738b29ed6c70a
Matt Rodgers <mattrodgers31@gmail.com>
2022-12-22 13:34:34 +0000

    day 14 pt2

--- d14/d14.py
+++ d14/d14.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python
 
 import sys
-from collections import deque
 import numpy as np
 
 
@@ -9,11 +8,8 @@ with open(sys.argv[1], "r") as f:
     lines = [[list(map(int, p.split(","))) for p in line.strip().split(" -> ")] for line in f.readlines()]
 
 
-maxx = 0
-minx = np.inf
 maxy = 0
-miny = np.inf
-filled = deque([])
+filled = set()
 for i, line in enumerate(lines):
     line_offset = line.copy()
     line_offset.pop(0)
@@ -24,21 +20,16 @@ for i, line in enumerate(lines):
         y = (start[1], end[1])
         for i in range(min(x), max(x) + 1):
             for j in range(min(y), max(y) + 1):
-                filled.append((i, j))
-                if i < minx:
-                    minx = i
-                if i > maxx:
-                    maxx = i
-                if j < miny:
-                    miny = j
+                filled.add((i, j))
                 if j > maxy:
                     maxy = j
 
-assert minx > 0
-assert miny > 0
 
-
-def visualise(minx, maxx, miny, maxy, filled):
+def visualise(filled):
+    minx = min([pt[0] for pt in filled])
+    miny = min([pt[1] for pt in filled])
+    maxx = max([pt[0] for pt in filled])
+    maxy = max([pt[1] for pt in filled])
     grid = np.zeros((maxy-miny+1, maxx-minx+1))
     for point in filled:
         pt_norm = (point[1] - miny, point[0] - minx)
@@ -49,7 +40,12 @@ def visualise(minx, maxx, miny, maxy, filled):
 
 def drop_one_sand(pos, maxy, filled):
     # Keep iterating until sand position exceeds max y co-ordinate
-    while pos[1] <= maxy:
+    start_pos = pos
+    floory = maxy + 2
+    while pos[1] < floory:
+        if pos[1] + 1 == floory:
+            filled.add(pos)
+            return filled, False
         # Try to drop directly down
         if (pos[0], pos[1] + 1) not in filled:
             pos = (pos[0], pos[1] + 1)
@@ -61,12 +57,15 @@ def drop_one_sand(pos, maxy, filled):
             pos = (pos[0] + 1, pos[1] + 1)
         # Blocked
         else:
-            filled.append(pos)
+            filled.add(pos)
+            if pos == start_pos:
+                return filled, True
             return filled, False # Not complete
 
+    assert(0)
     return filled, True
 
-
+n = len(filled)
 i = 0
 while True:
     filled, complete = drop_one_sand((500, 0), maxy, filled)
@@ -75,4 +74,4 @@ while True:
         break
     i += 1
 
-print(f"Part 1: {i}")
+print(f"Part 2: {len(filled) - n}")
