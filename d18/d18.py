2803fdb26bae38f57c950c3e18b46f20981e522a
Matt Rodgers <mattrodgers31@gmail.com>
2022-12-23 17:34:56 +0000

    day 18 pt2

--- d18/d18.py
+++ d18/d18.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 import sys
+from collections import deque
 
 
 with open(sys.argv[1], "r") as f:
@@ -24,3 +25,48 @@ for cx, cy, cz in cubes:
 
 pt1 = len(cubes) * 6 - adj_count
 print(f"Part 1: {pt1}")
+
+
+# Part 2
+
+def get_reachable_points(cubes):
+    # Get size of grid
+    x0 = min(c[0] for c in cubes) - 1
+    y0 = min(c[1] for c in cubes) - 1
+    z0 = min(c[2] for c in cubes) - 1
+    x1 = max(c[0] for c in cubes) + 1
+    y1 = max(c[1] for c in cubes) + 1
+    z1 = max(c[2] for c in cubes) + 1
+
+    # BFS
+    start = (x0, y0, z0) # Start from arbitrary position on edge
+    visited = set(start)
+    q = deque([start])
+
+    while q:
+        next = q.pop()
+        x, y, z = next
+        for ax, ay, az in ADJACENT:
+            nx = x + ax
+            ny = y + ay
+            nz = z + az
+            if nx >= x0 and nx <= x1 and ny >= y0 and ny <= y1 and nz >= z0 and nz <= z1:
+                neighbor = (nx, ny, nz)
+                if neighbor not in visited:
+                    if neighbor not in cubes:
+                        q.append(neighbor)
+                        visited.add(neighbor)
+
+    return visited
+
+reachable = get_reachable_points(cubes)
+
+surface_area = 0
+for cx, cy, cz in cubes:
+    for ax, ay, az in ADJACENT:
+        neighbor = (cx + ax, cy + ay, cz + az)
+        if neighbor not in cubes:
+            if neighbor in reachable:
+                surface_area += 1
+
+print(f"Part 2: {surface_area}")
